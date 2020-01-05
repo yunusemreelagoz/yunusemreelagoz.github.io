87a4833aea386870f041c64d1c5d306674d001ba
yunusemreelagoz <yunusemreelagoz@gmail.com>
2020-01-05 18:58:32 +0300

    yapay zeka denemesi
    ...

---
+++ precache-manifest.7502ed1149657a4d3ef00d494372bca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12de00761f0c36f59ce675548e399ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "968249b89fdee1631401",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f7a9cb484d7b9ee04b09",
+    "url": "/static/js/2.527efcd4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.527efcd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "968249b89fdee1631401",
+    "url": "/static/js/main.b29af18c.chunk.js"
+  },
+  {
+    "revision": "fa6362989293046416ad",
+    "url": "/static/js/runtime-main.dc4ceda8.js"
+  }
+]);
